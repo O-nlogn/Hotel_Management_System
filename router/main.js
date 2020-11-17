22ab4c20547a13101b8299fa4da373cc7910ec66
hihiroo <alsrud606@naver.com>
2020-11-17 21:16:50 +0900

    Feat: 로그인 성공 시 main.html로 연결

--- router/main.js
+++ router/main.js
@@ -1,45 +1,53 @@
 var qs = require('querystring');
 var dbconfig = require('../db');
 
-module.exports = function(app){
-    app.get('/', function(req,res){
+module.exports = function (app) {
+    app.get('/', function (req, res) {
         res.render('login.html');
     });
     app.get('/login', function (req, res) {
         res.render('login.html');
     });
-    app.get('/main', function(req, res){
+    app.get('/main', function (req, res) {
         res.render('main.html');
     });
 
-    app.post('/login_data', function(req, res){
+
+    /* 로그인 버튼을 눌렀을 때 request 처리 */
+    app.post('/login_data', function (req, res) {
         var body = '';
-        req.on('data', function(data){
+        req.on('data', function (data) {
             body = body + data;
         });
-        req.on('end', function(){
+        req.on('end', function () {
             var post = qs.parse(body);
             var id = post.id;
             var pw = post.pw;
-            
+
+            /* db에서 id, pw가 일치하는 직원이 있는지 검색 */
             dbconfig.connect();
             var sql = 'SELECT * FROM users WHERE id=? and password=?';
-            var params = [id,pw];
-            dbconfig.query(sql,params,function(err,rows,fields){
-                if(err){
+            var params = [id, pw];
+
+            dbconfig.query(sql, params, function (err, rows, fields) {
+                if (err) {
                     console.log(err);
+                    res.writeHead(200);
+                    res.end();
                 }
-                else{
-                    if(rows.length == 0){
-                        console.log('아이디나 비밀번호가 틀렸습니다.'); 
+                else {
+                    if (rows.length == 0) {
+                        console.log('아이디나 비밀번호가 틀렸습니다.');
+                        res.writeHead(200);
+                        res.end();
                     }
 
-                    else console.log('로그인 성공');
+                    else {
+                        res.render('main.html'); // 로그인 성공 시 main페이지 연결
+                        console.log('로그인 성공');
+                    }
                 }
             });
-
-            res.writeHead(200);
-            res.end();
         });
     });
 }
