041ac2504c09af5cc390c8b4045753617b804f9f
hhyuw <high2092@gmail.com>
2020-11-22 02:29:55 +0900

    Feat: 로그인 실패 시 표시될 텍스트 스타일 보완 및 테스트 API 파일 생성

---
+++ test.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+var server = app.listen(3000);
+
+app.get('/', function (req, res) {
+    res.render('main');
+});
+
+app.get('/room', function (req, res) {
+    res.render('room');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login', {login: 'true'});
+});
+
+app.get('/', function (req, res) {
+    res.render('main');
+});
+
+/* 이미지, css 등 정적파일에 접근하기 위해 public 폴더 추가*/
+app.use(express.static('public'));
