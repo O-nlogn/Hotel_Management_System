c566edf5511e4af47388d2b75fb89a99a3e29baf
hihiroo <alsrud606@naver.com>
2020-11-27 02:03:11 +0900

    Feat: 공지사항, 예약관리, 직원관리, 내정보, 비밀번호 변경 페이지 get request 추가
    + 주석 추가 및 코드 리팩토링

--- router/main.js
+++ router/main.js
@@ -2,41 +2,50 @@ var cp = require('cookie-parser');
 var dbconfig = require('../db');
 var ejs = require('ejs');
 
+
 module.exports = function (app) {
 
     app.use(cp());
     dbconfig.connect();
-    var postrouter = require('./post')(app);
+    var postrouter = require('./post')(app); // post방식 request처리 라우터 인클루드
+
 
     app.get('/', function (req, res) {
-        if (req.cookies.is_logged_in === undefined) {
-            res.redirect('/login');
-        } 
-        else if (req.cookies.is_logged_in === 'false') {
-            res.redirect('/login');
-        }
-        else {
+        if (req.cookies.is_logged_in === 'true'){ // 로그인 상태면 main페이지로, 아니면 로그인 페이지로 리다이렉트
             res.redirect('/main');
         }
+        else res.redirect('/login');
     });
 
+
+    /* 로그인 관련 */
     app.get('/login', function (req, res) {
-        res.render('login',{login:req.cookies.is_logged_in});
+        res.render('login',{login:req.cookies.is_logged_in}); // 로그인 실패시 UI를 다르게 하기 위해 쿠키를 넘겨줌
     });
 
     app.get('/logout', function (req, res) {
-        res.cookie('is_logged_in', undefined);
+        res.cookie('is_logged_in', undefined); // 로그인 쿠키를 지우고 로그인 페이지로 리다이렉트
         res.redirect('/login');
     });
 
+
+    /* 메뉴 관련 */
+    app.get('/notice', function (req, res) {
+        if (req.cookies.is_logged_in === 'true') {
+            res.render('notice');
+        }
+        else res.redirect('/login');
+    });
+
+    app.get('/reservation', function (req, res) {
+        if (req.cookies.is_logged_in === 'true') {
+            res.render('reservation');
+        }
+        else res.redirect('/login');
+    });
+
     app.get('/room', function (req, res) {
-        if (req.cookies.is_logged_in === undefined) {
-            res.redirect('/login');
-        }
-        else if (req.cookies.is_logged_in === 'false') {
-            res.redirect('/login');
-        }
-        else{
+        if (req.cookies.is_logged_in === 'true'){
             var sql = 'SELECT stay.room, users.name as staff_name, nationality, stay.personnel, CASE WHEN should_paid IS NULL THEN 0 ELSE should_paid END AS should_paid, cardkey, request, cleaning, checkin, checkout from stay'
             sql += ' JOIN responsibility ON stay.room = responsibility.room';
             sql += ' JOIN users ON stay.room = responsibility.room and users.id = responsibility.id';
@@ -69,22 +78,45 @@ module.exports = function (app) {
                 }
             });
         }
+        else res.redirect('/');
     });
 
-    app.get('/main', function (req, res) {
-        if (req.cookies.is_logged_in === undefined) {
-            res.redirect('/login');
-        } 
-        else if (req.cookies.is_logged_in === 'false') {
-            res.redirect('/login');
+    app.get('/staff', function (req, res) {
+        if (req.cookies.is_logged_in === 'true') {
+            res.render('staff');
         }
-        else {
+        else res.redirect('/login');
+    });
+
+
+
+    /* 메인페이지, 내정보, 비밀번호 수정 관련*/
+    app.get('/main', function (req, res) {
+        if (req.cookies.is_logged_in === 'true') {
             res.render('main');
         }
+        else res.redirect('/login');
     });
 
+    app.get('/mypage', function(req,res){
+        if (req.cookies.is_logged_in === 'true'){
+            res.render('mypage');
+        }
+        else res.redirect('/login');
+    });
+
+    app.get('/changepw', function (req, res) {
+        if (req.cookies.is_logged_in === 'true') {
+            res.render('changepw');
+        }
+        else res.redirect('/login');
+    });
+
+
+    
+    /* 테스트 관련*/
     app.get('/test', function (req, res) {
-        sql = 'SELECT * FROM TEXT'
+        var sql = 'SELECT * FROM TEXT'
         dbconfig.query(sql, (err, rows) => {
             console.log(new Date() + ' | testing : ' + rows.length);
             if (err) {
