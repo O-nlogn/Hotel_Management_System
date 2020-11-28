8bafda151804124046ae9b58e2ecb8997a2c1cd9
hhyuw <high2092@gmail.com>
2020-11-29 01:21:56 +0900

    Feat: 객실관리 페이지 css 개선

--- test.js
+++ test.js
@@ -18,6 +18,8 @@ app.get('/room', function (req, res) {
         [
             {number: 301, type: 'standard'},
             { number: 302, type: 'standard' },
+            { number: 303, type: 'standard' },
+            
             { number: 303, type: 'standard' }
         ],
 
@@ -25,6 +27,9 @@ app.get('/room', function (req, res) {
         [
             {room: 301, staff_name: '신민경', nationality: 'SouthKorea', personnel: 2, should_paid: 1000, cardkey: 0, request: '청소 깨끗이 해주세요', cleaning: 0,
                     checkin: '2020-11-21 14:00:00', checkout: '2020-11-25 11:00:00'},
+            {room: 303, staff_name: '홍길동', nationality: 'USA', personnel: 1, should_paid: 3000, cardkey: 1, request: '영어 가능 직원 불러주세요', cleaning: 1,
+            checkin: '2020-11-20 14:00:00', checkout: '2020-11-26 11:00:00'},
+            
             {room: 303, staff_name: '홍길동', nationality: 'USA', personnel: 1, should_paid: 3000, cardkey: 1, request: '영어 가능 직원 불러주세요', cleaning: 1,
             checkin: '2020-11-20 14:00:00', checkout: '2020-11-26 11:00:00'}
         ],
