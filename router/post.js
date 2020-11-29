1d0787b2e8e4a7f6131f9ee20943b43e61306328
JunSeong Park <rasauq1122@gmail.com>
2020-11-29 19:46:40 +0900

    Refactor: body-parser 모듈 사용에 따른 /login_data 구조 변경

--- router/post.js
+++ router/post.js
@@ -9,39 +9,33 @@ module.exports = function (app) {
 
     /* 로그인 버튼을 눌렀을 때 request 처리 */
     app.post('/login_data', function (req, res) {
-        var body = '';
-        req.on('data', function (data) {
-            body = body + data;
-        });
-        req.on('end', function () {
-            var post = qs.parse(body);
-            var id = post.id;
-            var pw = post.pw;
-            res.cookie('userID', id);
+        
+        var id = req.body.id;
+        var pw = req.body.pw;
+        res.cookie('userID', id);
 
-            /* db에서 id, pw가 일치하는 직원이 있는지 검색 */
-            var sql = 'SELECT * FROM users WHERE id=? and password=?';
-            var params = [id, crypto.createHash('sha512').update(pw).digest('hex')];
+        /* db에서 id, pw가 일치하는 직원이 있는지 검색 */
+        var sql = 'SELECT * FROM users WHERE id=? and password=?';
+        var params = [id, crypto.createHash('sha512').update(pw).digest('hex')];
 
-            dbconfig.query(sql, params, function (err, rows, fields) {
-                if (err) {
-                    console.log(err);
-                    res.writeHead(200);
-                    res.end();
+        dbconfig.query(sql, params, function (err, rows, fields) {
+            if (err) {
+                console.log(err);
+                res.writeHead(200);
+                res.end();
+            }
+            else {
+                if (rows.length == 0) {
+                    console.log('아이디나 비밀번호가 틀렸습니다.');
+                    res.cookie('is_logged_in', false);
                 }
                 else {
-                    if (rows.length == 0) {
-                        console.log('아이디나 비밀번호가 틀렸습니다.');
-                        res.cookie('is_logged_in', false);
-                    }
-                    else {
-                        console.log('로그인 성공');
-                        res.cookie('is_logged_in', true);
-                    }
+                    console.log('로그인 성공');
+                    res.cookie('is_logged_in', true);
                 }
+            }
 
-                res.redirect('/');
-            });
+            res.redirect('/');
         });
     });
 
