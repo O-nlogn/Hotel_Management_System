259ea371edd3b5c6975daa6e09cb2074819f7068
hhyuw <high2092@gmail.com>
2020-12-08 03:03:00 +0900

    Fix: 직원관리 페이지 프로필 크기 고정 File: db-template.js 추가

--- db-template.js
+++ db-template.js
@@ -4,7 +4,7 @@ var sql = require('mysql');
 const config = sql.createConnection({
     host: "localhost",
     user: "root", // your username
-    password: "HW8418635@", // your password
+    password: "1234", // your password
     database: "hotel_system",
     dateStrings: "date"
 });
